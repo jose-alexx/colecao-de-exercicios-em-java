987b83abb0f38b55cc67292a70ceea05e73744f0
alex <josealex41012@gmail.com>
2024-09-15 19:56:57 -0300

    adicionando texte

---
+++ Main.java
@@ -0,0 +1,35 @@
+/**
+ * Exercicio--01
+ */
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner entrada = new Scanner(System.in); // Definindo o Scanner
+        
+        int primeiro_Num, segundo_Num;
+
+        System.out.println("Digite o primeiro número: "); // Método Sistem.out 
+        primeiro_Num = entrada.nextInt();
+        
+        System.out.println("Digite o segundo número: ");
+        segundo_Num = entrada.nextInt();
+
+        int soma, subtracao, multiplicacao;
+        float divisao;
+
+        soma = primeiro_Num + segundo_Num;
+        subtracao = primeiro_Num - segundo_Num;
+        multiplicacao = primeiro_Num * segundo_Num;
+        divisao = (float) primeiro_Num / segundo_Num;
+
+        // https://dev.to/linivecristine/print-vs-println-vs-printf-qual-a-diferenca-5dk0
+        // https://www.bosontreinamentos.com.br/java/metodos-printf-print-e-println-curso-de-programacao-em-java/
+
+        System.out.print("\nSoma: " + soma + "\n");
+        System.out.printf("%s%d\n", "Subtração: ", subtracao);
+        System.out.println("Multiplicação: " + multiplicacao);
+        System.out.printf("%s%.2f", "Divisão: ", divisao);
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+1. Básico: Operações com variáveis
+     Escreva um programa que:
+
+     Leia dois números inteiros.
+Exiba o resultado da soma, subtração, multiplicação e divisão entre esses dois números.
+
+Exemplo de Entrada:
+
+   -> 5 
+   -> 3 
+
+Exemplo de Saída:
+   
+   Soma: 8
+   Subtração: 2
+   Multiplicação: 15
+   Divisão: 1.67
